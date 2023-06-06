60965473817a478a385aa9004305a9a338eec1ca
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-06-06 16:40:29 +0600

    associative arrays
    simple associative array

---
+++ associative_arrays.php
@@ -0,0 +1,13 @@
+<?php
+// associative arrays
+  $capitals = array(
+    "USA" => "Washington D.C",
+    "Bangladesh" => "Dhaka",
+    "India" => "Delhi",
+    "Japan" => "Kyoto"
+  );
+
+  foreach($capitals as $key => $value)
+  echo $capitals["Bangladesh"];
+
+?>
