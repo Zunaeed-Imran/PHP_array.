7ff49939f5fc590349b7f61020d4a20c98a38b57
imran <zunaeede@gmail.com>
2023-11-28 17:52:41 +0600

    main - add some important note

--- associative_arrays2.php
+++ associative_arrays2.php
@@ -1,5 +1,6 @@
 <?php
 // associative arrays
+
   $capitals = array(
     "USA" => "Washington D.C",
     "Bangladesh" => "Dhaka",
@@ -7,8 +8,8 @@
     "Japan" => "Kyoto"
   );
 
-  $capitals["USA"] = "Las Vagas";
-  $capitals["Chaina"] = "Beijing";
+  $capitals["USA"] = "Las Vagas";   // we overwrite "USA"
+  $capitals["Chaina"] = "Beijing";  // we add a value in array
 
   foreach($capitals as $key => $value)
   echo "{$key} = {$value} </br>";
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 ## Array Practice in PHP
+
+##### associative_array2.php 
+- in that example we use associative array over_wrrite and replace mathod
