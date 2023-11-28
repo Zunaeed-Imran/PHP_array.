efc96f8c0161a6830a0e1efff943f8e31709d10c
imran <zunaeede@gmail.com>
2023-11-28 16:47:38 +0600

    practice - practice some print

---
+++ 1_practice.php
@@ -0,0 +1,10 @@
+<?php
+// Practice random array
+
+$array_test = array('YAHAMA', 'PULSER', 'GIXXER');
+
+echo $array_test[0]. '<br>';
+echo $array_test[1]. '<br>';
+echo $array_test[2]. '<br>';
+
+?>
