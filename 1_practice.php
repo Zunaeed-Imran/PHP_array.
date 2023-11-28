91783af517391ebc4e3352e5ee3be7da0c1bd976
imran <zunaeede@gmail.com>
2023-11-28 16:50:47 +0600

    practice - use var dump to print

--- 1_practice.php
+++ 1_practice.php
@@ -3,8 +3,8 @@
 
 $array_test = array('YAHAMA', 'PULSER', 'GIXXER');
 
-echo $array_test[0]. '<br>';
-echo $array_test[1]. '<br>';
+var_dump($array_test[0]);
+echo '<br>'. $array_test[1]. '<br>';
 echo $array_test[2]. '<br>';
 
 ?>
